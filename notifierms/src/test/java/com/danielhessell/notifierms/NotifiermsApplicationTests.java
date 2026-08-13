516e7bed30263f8419388ab657a759081c047baf
Daniel Hessel <danieldaniabreu@gmail.com>
2026-08-13 05:16:50 -0300

    chore: setup inicial

---
+++ notifierms/src/test/java/com/danielhessell/notifierms/NotifiermsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielhessell.notifierms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifiermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
