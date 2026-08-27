6aaddf2edb8def7ef32dff530285399493f01003
Daniel Hessel <danieldaniabreu@gmail.com>
2026-08-27 20:02:37 -0300

    chore(orderms-btgpactual): setup projeto

---
+++ orderms-btgpactual/src/main/java/br/com/danielhessel/orderms_btgpactual/OrdermsBtgpactualApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielhessel.orderms_btgpactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermsBtgpactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermsBtgpactualApplication.class, args);
+	}
+
+}
---
+++ orderms-btgpactual/src/test/java/br/com/danielhessel/orderms_btgpactual/OrdermsBtgpactualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielhessel.orderms_btgpactual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermsBtgpactualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
